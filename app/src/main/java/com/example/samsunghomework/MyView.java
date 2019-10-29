39083f1b1ead7b36724e211afcc014dbc77f3d15
Lev <ozherelyevlev@gmail.com>
2019-10-29 21:05:55 +0300

    1.9.5

--- app/src/main/java/com/example/samsunghomework/MyView.java
+++ app/src/main/java/com/example/samsunghomework/MyView.java
@@ -2,35 +2,58 @@ package com.example.samsunghomework;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 
-
-
 public class MyView extends View {
-
-    MyView(Context context) {
-        super(context);
-
+    float[] x;
+    float[] y;
+    float[] vx;
+    float[] vy;
+    int N = 50;
+    void addValues(float[] array, float[] values){
+        for (int i = 0; i < array.length; i++){
+            array[i] += values[i];
+        }
     }
+    public MyView(Context context) {
 
-
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(9);
-        int y = 0;
-        while (y < canvas.getHeight()) {
-            canvas.drawLine(0, y,
-                    this.getWidth(), y, paint);
-            y += 50;
+        super(context);
+        fillRandom(x, 0, 500);
+        fillRandom(y, 0, 500);
+        fillRandom(vx, -3, 3);
+        fillRandom(vy, 500, 900);
         }
 
-
+    float rand(float min , float max){
+        return (float)(Math.random() * (max - min + 1)) + min;
     }
+    void fillRandom(float[] array , float min, float max){
+        for (int i = 0; i < array.length; i++){
+            array[i] = rand (min, max);
+        }
+    }
+
+    Paint paint = new Paint();
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+
+        // готовим массивы x и у для следущего кадра
+        addValues(x, vx);
+        addValues(y, vy);
+        //запрашиваем перерисовку
+        invalidate();
+    }
+
+
+
+
 }
+
 
 
 
