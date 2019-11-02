cf6eb271ce7f2f4b1f730782fc762757ea8d8601
hhh <ania.daa11@gmail.com>
2019-11-02 10:17:46 +0300

    1.9.5

--- app/src/main/java/com/example/samsunghomework/MyView.java
+++ app/src/main/java/com/example/samsunghomework/MyView.java
@@ -6,25 +6,12 @@ import android.graphics.Paint;
 import android.view.View;
 
 public class MyView extends View {
-    float[] x;
-    float[] y;
-    float[] vx;
-    float[] vy;
-    int N = 50;
-    void addValues(float[] array, float[] values){
-        for (int i = 0; i < array.length; i++){
-            array[i] += values[i];
-        }
-    }
-    public MyView(Context context) {
 
-        super(context);
-        fillRandom(x, 0, 500);
-        fillRandom(y, 0, 500);
-        fillRandom(vx, -3, 3);
-        fillRandom(vy, 500, 900);
-        }
-
+    int N = 50; // количество шариков
+    float[] x = new float[N];
+    float[] y = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
     float rand(float min , float max){
         return (float)(Math.random() * (max - min + 1)) + min;
     }
@@ -34,26 +21,38 @@ public class MyView extends View {
         }
     }
 
-    Paint paint = new Paint();
-    @Override
-    protected void onDraw(Canvas canvas){
+    public MyView(Context context) {
+        super(context);
+        fillRandom(x, 0, 500);
+        fillRandom(y, 0, 500);
+        fillRandom(vx, -3, 3);
+        fillRandom(vy, 500, 0);
 
-        for (int i = 0; i < N; i++) {
-            canvas.drawCircle(x[i], y[i], 20, paint);
+    }
+
+    void add(float[] array, float[] values) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] += values[i];
         }
+    }
 
-        // готовим массивы x и у для следущего кадра
-        addValues(x, vx);
-        addValues(y, vy);
-        //запрашиваем перерисовку
+    Paint paint = new Paint();
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // отрисовываем все шарики
+        drawBalls(canvas);
+        // готовим массивы x и у для следующего кадра
+        add(x, vx);
+        add(y, vy);
+        // запрашиваем перерисовку
         invalidate();
     }
 
-
-
-
+    private void drawBalls(Canvas canvas) {
+        for (int i = 0; i < N; i++) {
+            canvas.drawCircle(x[i], y[i], 20, paint);
+        }
+    }
 }
-
-
-
 
